eddcdd923406a68f3bd89513839ceeacd9068ea8
santhos <santhosprabahar@gmail.com>
2016-06-19 23:19:54 +0530

    initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var portno = 3000;
+
+var middleware = {
+	requireauthentication : function (req , res , next) {
+		console.log( "private route hit");
+		next();
+	},
+	logger : function( req, res, next){
+
+		console.log('request :' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+
+	}
+
+	
+};
+
+app.use(middleware.logger);
+app.get('/about',middleware.requireauthentication, function (req, res) {
+	
+	res.send("hello prabahar");
+	// body...
+});
+
+app.use(express.static(__dirname + '/public'));
+app.listen(portno, function ()
+{
+  		console.log("server is up and running at " + portno );
+  			});
